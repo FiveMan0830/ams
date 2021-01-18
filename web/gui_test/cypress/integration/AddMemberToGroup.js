710a3e9c5f279fc159c67962a17b6954f588ffd9
Chih-chi Li <t107590452@ntut.org.tw>
2021-01-18 13:45:55 +0800

    add member gui test

---
+++ web/gui_test/cypress/integration/AddMemberToGroup.js
@@ -0,0 +1,59 @@
+
+describe("Add a member to the group", () => {
+    const inputTeam = 'Test';
+    const inputUsername = 'test'
+
+    it("Visit the website", ()=>{
+
+        cy.visit("http://127.0.0.1:5500/web/member.html");
+    });
+
+    it("Enter Team's name and username", () => {
+        const teamText = '//input[@id="groupname-field"]';
+        const usernameText = '//input[@id="username-field"]';
+        const addBtn = '//input[@id="add-button"]';
+
+        cy.xpath(teamText)
+          .type(inputTeam)
+          .should("have.value",inputTeam);
+
+        cy.xpath(usernameText)
+          .type(inputUsername)
+          .should("have.value",inputUsername);
+
+        cy.xpath(addBtn).click();
+    });
+
+
+    it("Assert Team", () =>{
+        const groupList = '//ul[@id="groups"]'
+        const searchBtn = '//input[@id="get-member-button"]';
+
+        cy.xpath(searchBtn).click();
+
+        cy.xpath(groupList)
+          .should(($li)=>{
+            expect($li).to.contain(inputUsername)
+          });
+    })
+
+
+    it("Tear down", () => {
+        const teamText = '//input[@id="groupname-field"]';
+        const usernameText = '//input[@id="username-field"]';
+        const removeBtn = '//input[@id="remove-button"]';
+
+        cy.xpath(teamText)
+          .type(inputTeam)
+          .should("have.value",inputTeam);
+
+        cy.xpath(usernameText)
+          .type(inputUsername)
+          .should("have.value",inputUsername);
+
+        cy.xpath(removeBtn).click();
+    });
+
+
+
+});
