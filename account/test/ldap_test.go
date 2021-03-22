e6a4a959f5bd835bc80e0d18106c83c947a749c9
FiveMan0830 <t107590013@ntut.org.tw>
2021-03-22 15:16:28 +0800

    UT using struct

--- account/test/ldap_test.go
+++ account/test/ldap_test.go
@@ -8,6 +8,21 @@ import (
 	"ssl-gitlab.csie.ntut.edu.tw/ois/ois-project/ams/account"
 )
 
+type user struct {
+	ID string
+	name string
+	givenName string
+	surname string
+	password string
+	email string
+}
+
+type group struct {
+	name string
+	groupLeaderUsername string
+	ID string
+}
+
 const adminUser string = "admin"
 const adminPassword string = "admin"
 
@@ -85,20 +100,31 @@ func TestMain(m *testing.M) {
 }
 
 func setup() {
+	user1 := user{"c61965be-8176-4419-b289-4d52617728fb", "fiveman123", "Richard", "Zheng", "richard123", "richardEmail@gmail.com"}
+	user2 := user{"c56515be-1654-7895-1564-1d52513528cf", "audi98", "Audi", "Wu", "audipm984", "audiEmail@gmail.com"}
+	//user3 := user{"a56515be-5783-8738-1564-1d52513528cz", "stella83", "Stella", "Chen", "stella423", "stellaEmail@gmail.com"}
+
+	leader1 := user{"a34531da-8563-9517-3578-3e38754896dg", "george88", "George", "Lim", "george189", "georgeEmail@gmail.com"}
+	leader2 := user{"b96875kl-6842-7539-8549-2c56482648fa", "david93", "David", "Wang", "david632", "davidEmail@gmail.com"}
+	leader3 := user{"c16475kl-6762-7489-8359-2c56846648fm", "sherry99", "Sherry", "Ye", "sherry753", "sherryEmail@gmail.com"}
+
+	group1 := group{"OIS", "david93", "d23475kl-4862-7456-8473-2c53916648fn"}
+	group2 := group{"SSL", "george88", "e16987kl-9512-7424-9629-2c56884248lm"}
+	// group3 := group{"KanBan", "sherry88", "f16846kl-7862-4539-1684-3a56884248lo"}
+
 	accountManagement := account.NewLDAPManagement()
+	accountManagement.CreateUser(adminUser, adminPassword, user1.ID, user1.name, user1.givenName, user1.surname, user1.password, user1.email)
+	accountManagement.CreateUser(adminUser, adminPassword, user2.ID, user2.name, user2.givenName, user2.surname, user2.password, user2.email)
 
-	accountManagement.CreateUser(adminUser, adminPassword, userID, username, givenName, surname, userPassword, userEmail)
-	accountManagement.CreateUser(adminUser, adminPassword, userID2, username2, givenName2, surname2, userPassword2, userEmail2)
-
-	accountManagement.CreateUser(adminUser, adminPassword, leaderID, leaderUsername, leaderGivenName, leaderSurname, leaderPassword, leaderEmail)
-	accountManagement.CreateUser(adminUser, adminPassword, leaderID2, leaderUsername2, leaderGivenName2, leaderSurname2, leaderPassword2, leaderEmail2)
-	accountManagement.CreateUser(adminUser, adminPassword, leaderID3, leaderUsername3, leaderGivenName3, leaderSurname3, leaderPassword3, leaderEmail3)
+	accountManagement.CreateUser(adminUser, adminPassword, leader1.ID, leader1.name, leader1.givenName, leader1.surname, leader1.password, leader1.email)
+	accountManagement.CreateUser(adminUser, adminPassword, leader2.ID, leader2.name, leader2.givenName, leader2.surname, leader2.password, leader2.email)
+	accountManagement.CreateUser(adminUser, adminPassword, leader3.ID, leader3.name, leader3.givenName, leader3.surname, leader3.password, leader3.email)
 
 	accountManagement.CreateOu(adminUser, adminPassword, ouName)
 	accountManagement.CreateOu(adminUser, adminPassword, "OISGroup")
 
-	accountManagement.CreateGroup(adminUser, adminPassword, groupName, groupLeaderUsername, groupID)
-	accountManagement.CreateGroup(adminUser, adminPassword, groupName2, groupLeaderUsername2, groupID2)
+	accountManagement.CreateGroup(adminUser, adminPassword, group1.name, group1.groupLeaderUsername, group1.ID)
+	accountManagement.CreateGroup(adminUser, adminPassword, group2.name, group2.groupLeaderUsername, group2.ID)
 
 	fmt.Printf("\033[1;36m%s\033[0m", "> Setup completed\n")
 }
