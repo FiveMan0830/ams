1059423c5484f7917fc21f83cac8fffe2347e92e
Mashu <ndsl6211@gmail.com>
2022-01-31 22:07:33 +0800

    set cors origin from env var

--- pkg/gin.go
+++ pkg/gin.go
@@ -2,6 +2,8 @@ package pkg
 
 import (
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -9,14 +11,12 @@ import (
 
 func NewGinEngine() *gin.Engine {
 	engine := gin.Default()
+
 	engine.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:8000", "http://localhost:30000", "http://localhost:3000"},
+		AllowOrigins:     strings.Split(os.Getenv("CORS_ALLOWED_ORIGINS"), ","),
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "withCredentials"},
 		AllowCredentials: true,
-		AllowOriginFunc: func(origin string) bool {
-			return origin == "http://localhost:8000" || origin == "http://localhost:30000" || origin == "http://localhost:3000"
-		},
 	}))
 
 	engine.GET("/api/v2/cookie", func(c *gin.Context) {
